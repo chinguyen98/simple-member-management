f75239cd00c915446405e0b1d61b6418146713a5
ChiNguyen <dacchi6761@gmail.com>
2019-09-08 18:14:14 +0700

    Add register page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 //Routers
 const indexRouter = require('./routes/index.route');
+const registerRouter = require('./routes/register.route');
 
 const port = 3000;
 
@@ -13,15 +14,16 @@ app.set('views', './views');
 app.engine('handlebars', handlebars({ defaultLayout: 'layout' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', indexRouter);
+app.use('/', indexRouter);
+app.use('/register', registerRouter);
 
 //Handle Error
 app.use((req, res, next) => {
-    var err = new Error('Not Found!!!');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found!!!');
+	err.status = 404;
+	next(err);
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at port ${port}`);
+	console.log(`Server is running at port ${port}`);
 })
